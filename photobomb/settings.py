a7831851ae795caac073836639e9c2bbcb3a597d
Nico Burns <nico@nicoburns.com>
2018-09-25 13:04:53 +0100

    Q key

--- photobomb/settings.py
+++ photobomb/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b!k!z1mb1^tn$zk3h509wi*r(p(6-%0y5(n$(^3hvvuzlfhk!(erioihergigoi'
+SECRET_KEY = 'b!k!z1mb1^tn$zk3h509wi*r(p(6-%0y5(n$(^3hvvuzlfhk!(erioihergigoiQQQQQQ'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
